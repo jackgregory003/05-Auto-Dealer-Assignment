702b8f477365bfa55b3a5606f7ad378ab32d3d62
Jack Gregory <4balancingrocks@gmail.com>
2025-03-11 10:10:50 -0600

    starting on stars

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 const navElement = document.querySelector('#theNav');
 const btnElement = document.querySelector('#theButton');
+const myTarget = document.querySelector('#cards'); //create an empty card
+
 
 btnElement.addEventListener('click', () => {
     btnElement.classList.toggle('open');
@@ -8,3 +10,43 @@ btnElement.addEventListener('click', () => {
 
 //grab REVIEW data from json file
 import { reviews } from "../data/reviews.js";
+console.log(reviews);
+
+for (let i=0; i < reviews.length; i++)
+{
+console.log(i);
+
+//create a section
+const mySection = document.createElement('section')
+const myName = document.createElement('h3')
+myName.textContent = reviews[i].name
+
+const myReview = document.createElement('p')
+myReview.textContent = reviews[i].text
+
+
+//create images for star ratings
+console.log(reviews[i].stars)
+
+const starIndicator = document.createElement('img')
+starIndicator.src = "../images/star-outline.svg"
+//for {let step = 0; step<reviews[i].stars; step++}
+
+
+
+
+
+
+
+
+
+//build the figure with child elements
+mySection.appendChild(myName);
+mySection.appendChild(myReview)
+
+//add new card to the page
+myTarget.appendChild(mySection)
+
+}//end of loop
+
+
